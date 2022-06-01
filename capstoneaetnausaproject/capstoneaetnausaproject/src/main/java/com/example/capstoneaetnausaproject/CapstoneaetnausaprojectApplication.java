75e2674e934311adf7323a17c18f628748f15ae9
varshika <vani212008@gmail.com>
2022-06-01 17:03:06 -0400

    added files

---
+++ capstoneaetnausaproject/capstoneaetnausaproject/src/main/java/com/example/capstoneaetnausaproject/CapstoneaetnausaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.capstoneaetnausaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneaetnausaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneaetnausaprojectApplication.class, args);
+	}
+
+}
---
+++ capstoneaetnausaproject/capstoneaetnausaproject/src/test/java/com/example/capstoneaetnausaproject/CapstoneaetnausaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstoneaetnausaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneaetnausaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
